acf89573f0a0d7db72872ab7bbc1672c953e92d1
kasyap dharanikota <76957823+kasyap1234@users.noreply.github.com>
2024-04-13 09:57:26 +0530

    adding registration

--- auth/auth_handlers.go
+++ auth/auth_handlers.go
@@ -2,25 +2,53 @@
 package auth
 
 import (
+	"context"
+	"fmt"
+
 	"github.com/gin-gonic/gin"
+	"github.com/kasyap1234/eduhub_backend_golang/database"
 	model "github.com/kasyap1234/eduhub_backend_golang/models"
 	"go.mongodb.org/mongo-driver/mongo"
+	"gopkg.in/mgo.v2/bson"
 )
-var (
-	jwtKey =[]byte("your_secret_key")
-	mongoURI = "mongodb://localhost:27017"
-	dbName="college"
-	collectionName="users"
-	client *mongo.Client
-	collection *mongo.Collection
 
+var (
+	jwtKey         = []byte("your_secret_key")
+	mongoURI       = "mongodb://localhost:27017"
+	dbName         = "college"
+	collectionName = "users"
+	client         *mongo.Client
+	collection     *mongo.Collection
 )
-func init(){
-	collection :=client.Database(dbName).Collection(collectionName)
+
+func init() {
+	collection := client.Database(dbName).Collection(collectionName)
+	fmt.Print(collection)
+}
+func RegisterUser(c *gin.Context) {
+	var newUser model.User
+	if err := c.ShouldBindJSON(&newUser); err != nil {
+		c.JSON(400, gin.H{"error": "Invalid request data"})
+		return
+	}
+	// check if username already exists in the database ;
+	var existingUser model.User
+	err := collection.FindOne(context.Background(), bson.M{"username": newUser.Username}).Decode(&existingUser)
+	if err == nil {
+		c.JSON(400, gin.H{"error": "username already exists"})
+		return
+	}
+	// hash password ;
+	collection := database.GetMongoClient().Database("college").Collection("auth")
+	err = database.InsertOne(collection, newUser)
+
+	if err != nil {
+		c.JSON(500, gin.H{"error": "Failed to create user"})
+		return
+	}
+	c.JSON(200, gin.H{"message": "User created successfully"})
 
 }
-func RegisterUser(c *gin.Context){
-	var newUser model.User
-if err := c.ShouldBindJSON()
+func LoginUser(c *gin.Context) {
 
 }
