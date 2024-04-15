e5ef9f93bb5d487c742332a59dfd61a064b02bd7
kasyap dharanikota <76957823+kasyap1234@users.noreply.github.com>
2024-04-15 14:31:12 +0530

    blogs crud completed

--- handlers/blog_handlers.go
+++ handlers/blog_handlers.go
@@ -25,7 +25,61 @@ func addBlog(c *gin.Context){
 	}
 	c.JSON(http.StatusOK, gin.H{"message": "Blog added successfully"})
 }
+func getBlogByID(c *gin.Context){
+ blogID :=c.Param("ID")
+ objID,err :=primitive.ObjectIDFromHex(blogID)
+ if err !=nil {
+	 c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid blog ID"})
+	 return
 
+ }
+ collection :=database.GetMongoClient().Database("college").Collection("blogs")
+ filter :=bson.D{{"ID",objID}}
+ blog,err :=database.FindOneById(collection,filter); 
+ if err !=nil {
+	c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to fetch blog"})
+	return 
+ }
+ c.JSON(http.StatusOK, blog)
+
+
+}
+func deleteBlogByID(c *gin.Context){
+	blogId :=c.Param("ID")
+	objId,err :=primitive.ObjectIDFromHex(blogId)
+	if err!=nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid blog ID"})
+		return
+	}
+	collection :=database.GetMongoClient().Database("college").Collection("blogs")
+	filter :=bson.D{{"ID",objId}}
+	err :=database.DeleteOne(collection,filter); 
+	if err !=nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to delete blog"})
+		return 
+	}
+	c.JSON(http.StatusOK, gin.H{"message": "Blog deleted successfully"})
+
+}
 func UpdateBlog(c *gin.Context){
+    blogId :=c.Param("ID")
+	objId,err :=primitive.ObjectIDFromHex(blogId)
+	if err !=nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid blog ID"})
+		return
+	}
+	var updatedBlog model.Blog
+	if err := c.BindJSON(&updatedBlog); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid request body"})
+		return
+	}
+	collection :=database.GetMongoClient().Database("college").Collection("blogs")
+	filter :=bson.D{{"ID",objID}}
+	update :=bson.D{{"$set",bson.D{{"title",updatedBlog.Title},{"text",updatedBlog.Text},{"author",updatedBlog.Author}}}}
+	err =database.UpdateOne(collection,filter,update);
+	if err !=nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to update blog"})
+		return 
+	}
 	
 }
